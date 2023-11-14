338c88ef29aec15a026387cfef673311b9a8df9a
mayankp30 <mayankpurohit@outlook.com>
2023-11-14 13:09:03 -0500

    initial commit

---
+++ src/WeatherService.js
@@ -0,0 +1,3 @@
+const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+const WEATHER_API_KEY = ''; //open weater api key
