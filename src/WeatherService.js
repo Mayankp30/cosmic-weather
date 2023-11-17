1cc4a3f013b89165594ae7c21a708e435f6ef390
Mayank Purohit <Mayankp30@users.noreply.github.com>
2023-11-17 02:00:48 -0500

    added validation and documentation

--- src/WeatherService.js
+++ src/WeatherService.js
@@ -1,7 +1,19 @@
-const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
-const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = '70085da72ee43283daa761fe55c3ab67';
+/**
+ * OpenWeatherMap API key and base URL.
+ */
+const API_KEY = '70085da72ee43283daa761fe55c3ab67';
+const API_BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
+/**
+ * OpenWeatherMap API endpoints.
+ */
+const WEATHER_PATH = '/weather';
+const FORECAST_PATH = '/forecast';
+
+/**
+ * GeoDB API information.
+ */
+const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 const GEO_API_OPTIONS = {
   method: 'GET',
   headers: {
@@ -11,71 +23,98 @@ const GEO_API_OPTIONS = {
 };
 
 /**
- * Utility function to handle API requests.
- *
- * @param {string} url - The URL of the API endpoint.
- * @returns {Promise} A promise that resolves with the JSON response.
- * @throws {Error} If the API request fails or the response is not OK.
- */
-async function fetchFromAPI(url) {
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`API request failed with status: ${response.status}`);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-}
-
-/**
- * Fetch current weather and 5-day forecast data for a specific location.
+ * Fetches weather data for a given latitude and longitude.
  *
  * @param {number} lat - Latitude of the location.
  * @param {number} lon - Longitude of the location.
- * @returns {Promise} A promise that resolves with an array of weather and forecast data.
- * If an error occurs, it returns null.
+ * @returns {Promise<Array>} A promise that resolves with an array of weather and forecast data.
+ * @throws {Error} If the API request fails or the response is not OK.
  */
-export async function fetchWeatherData(lat, lon) {
+const fetchWeatherData = async (lat, lon) => {
   try {
-    const weatherUrl = `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
-    const forecastUrl = `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
+    // Fetch weather and forecast data in parallel
+    const [weatherPromise, forecastPromise] = await Promise.all([
+      fetch(getWeatherURL(lat, lon)),
+      fetch(getForecastURL(lat, lon)),
+    ]);
 
+    // Parse the JSON responses
     const [weatherResponse, forecastResponse] = await Promise.all([
-      fetchFromAPI(weatherUrl),
-      fetchFromAPI(forecastUrl),
+      weatherPromise.json(),
+      forecastPromise.json(),
     ]);
 
     return [weatherResponse, forecastResponse];
   } catch (error) {
     console.error(error);
-    return null;
+    throw new Error('Error fetching weather data.');
   }
-}
+};
 
 /**
- * Search for cities based on user input.
+ * Constructs the URL for weather data.
+ *
+ * @param {number} lat - Latitude of the location.
+ * @param {number} lon - Longitude of the location.
+ * @returns {string} The constructed weather URL.
+ */
+const getWeatherURL = (lat, lon) => {
+  return `${API_BASE_URL}${WEATHER_PATH}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+};
+
+/**
+ * Constructs the URL for forecast data.
+ *
+ * @param {number} lat - Latitude of the location.
+ * @param {number} lon - Longitude of the location.
+ * @returns {string} The constructed forecast URL.
+ */
+const getForecastURL = (lat, lon) => {
+  return `${API_BASE_URL}${FORECAST_PATH}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+};
+
+/**
+ * Fetches city data based on user input.
  *
  * @param {string} input - User's input (city name or coordinates).
- * @returns {Promise} A promise that resolves with city data.
+ * @returns {Promise<*>} A promise that resolves with city data.
  * @throws {Error} If the city API request fails or the response is not OK.
  */
-export async function fetchCities(input) {
+const fetchCities = async (input) => {
   try {
-    const citiesUrl = `${GEO_API_URL}/cities?minPopulation=10000&namePrefix=${input}`;
-    const response = await fetch(citiesUrl, GEO_API_OPTIONS);
+    // Validate input
+    if (!isValidInput(input)) {
+      // Return null if input is empty
+      return null;
+    }
+
+    // Declare response variable outside of the if block
+    let response = await fetch(
+      `${GEO_API_URL}/cities?minPopulation=10000&namePrefix=${input}`,
+      GEO_API_OPTIONS
+    );
 
     if (!response.ok) {
       throw new Error(`City API request failed with status: ${response.status}`);
     }
 
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
-    throw error;
+    console.error('Error fetching city data:', error);
+    throw new Error('Error fetching city data. Please try again later.');
   }
-}
+};
+
+/**
+ * Validates the input string.
+ *
+ * @param {string} input - User's input.
+ * @returns {boolean} True if the input is valid, false otherwise.
+ */
+const isValidInput = (input) => {
+  // Validate input
+  return input && input.trim().length > 0;
+};
+
+export { fetchWeatherData, fetchCities };
